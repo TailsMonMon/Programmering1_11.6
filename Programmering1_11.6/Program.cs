420dd5f2b94f6cc7e8c7c396000d51063d00f901
Monica Björk <m.bjork95@gmail.com>
2018-04-22 13:29:56 +0200

    Add project files.

---
+++ Programmering1_11.6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmering1_11._6 {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Skriv en procent i decimal form: ");
+            decimal precentageDecimal = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine(ToPercentage(precentageDecimal));
+            Console.ReadLine();
+        }
+
+        //      DECIMAL TO PERCENT
+        static string ToPercentage(decimal percentageDecimal) {
+            decimal percentageCalculate = percentageDecimal * 100;
+            string percentage = "Procenten är " + percentageCalculate;
+            return percentage;
+        }
+    }
+}
